9db9e1556279a9ab02bfe9bb830710837804ffc5
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-10 08:01:29 +0600

    show item searched

--- app.js
+++ app.js
@@ -14,18 +14,19 @@ const loadData = input => {
 const displayData = data => {
     const displayItems = document.getElementById('display-items');
     data.forEach(item => {
+        console.log(item)
         const div = document.createElement('div');
         div.classList.add('col')
         div.innerHTML = `
         <div class="card h-100">
-        <img src="${item.strMealThumb}" class="card-img-top" alt="...">
-        <div class="card-body">
-        <h5 class="card-title">${item.strMeal}</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to                        additional content. This card has even longer content than the first to show that equal height action.</p>
-        </div>
-        <div class="card-footer">
-        <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
+            <img src="${item.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${item.strMeal}</h5>
+                <p class="card-text">${item.strInstructions.slice(0, 200)}</p>
+            </div>
+            <div class="card-footer">
+                <button class="">Details</button>
+            </div>
         </div>
         `;
         displayItems.appendChild(div);
